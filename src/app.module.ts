a09f9df586a2a67320a659c039e34bc895bacc22
Jean Junior <jclgjunior@gmail.com>
2021-09-21 11:33:21 -0300

    Create Kafka Producer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { CheckoutController } from './checkout/checkout.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -18,8 +20,22 @@ import { ProductsModule } from './products/products.module';
       database: process.env.TYPEORM_DATABASE,
       entities: [process.env.TYPEORM_ENTITIES],
     }),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['host.docker.internal:9094'],
+          },
+          consumer: {
+            groupId: 'nestjs-kafka-example ' + Date.now(),
+          },
+        },
+      },
+    ]),
   ],
-  controllers: [AppController],
+  controllers: [AppController, CheckoutController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/checkout/checkout.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+
+@Controller('checkout')
+export class CheckoutController implements OnModuleInit {
+  private kafkaProducer: Producer;
+
+  constructor(
+    @Inject('KAFKA_SERVICE')
+    private readonly clientKafka: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    this.kafkaProducer = await this.clientKafka.connect();
+  }
+
+  @Get()
+  async checkout() {
+    const result = await this.kafkaProducer.send({
+      topic: 'pagamentos',
+      messages: [
+        {
+          key: Date.now().toString(),
+          value: 'Hello World ' + Date.now(),
+        },
+      ],
+    });
+    return 'Checkout';
+  }
+}
