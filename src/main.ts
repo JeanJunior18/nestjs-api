dec6106cc2ffc7646961a230c0c85e3118851210
Jean Junior <jclgjunior@gmail.com>
2021-09-23 15:17:17 -0300

    Kafka config on micrroserrvices

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './exception.filters/entity-not-found.exception-filter';
+import { KafkaOptions } from './products/Infrastructure/eventBUS/kafka.config';
 import { initSwagger } from './shared/plugins/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   initSwagger(app);
-  app.connectMicroservice({
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        brokers: ['host.docker.internal:9094'],
-      },
-      consumer: {
-        groupId: 'nestjs-kafka-example ' + Date.now(),
-      },
-    },
-  });
+  app.connectMicroservice(KafkaOptions);
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
   await app.startAllMicroservices();
